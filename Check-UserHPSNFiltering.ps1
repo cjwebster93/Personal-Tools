3a52aba53d469a6984d2a4549b66d9be45374512
Craig Webster <craigwebster14@gmail.com>
2020-04-10 11:58:53 +0100

    Added user check for UPN domain

--- Check-UserHPSNFiltering.ps1
+++ Check-UserHPSNFiltering.ps1
@@ -6,15 +6,24 @@ $user = Read-Host -Prompt 'Which user do we need to check? Enter their username'
 $domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain();
 $dcs    = $domain -Split "\@";
 
+$Verify = Read-Host ("Detected domain is $dcs. Is that correct? (Y/N)")
+if ($Verify -eq 'N') {
+    $dcs = Read-Host ("Please enter correct UPN (excluding '@')")
+    if ($dcs -contains '@') {
+        $dcs = $dcs -Split "\@"
+    }
+}
+
+#Loop to be inserted here to handle a user list
+
+#Get the user information, particularly UPN and Group Memberships
 $userinfo = Get-ADUser -Identity $user -Properties UserPrincipalName, MemberOf;
-Write-Host ($userinfo);
 
 #Check UPN
 $UPNcheck = ($userinfo.SamAccountName + "@$dcs")
-$UPNcheck
 
 if ($userinfo.UserPrincipalName -cnotmatch $UPNcheck) {
-    Set-ADUser -Identity $userinfo.SamAccountName -UserPrincipalName (($user.SamAccountName) + '@' + $dcs);
+    Set-ADUser -Identity $userinfo.SamAccountName -UserPrincipalName (($userinfo.SamAccountName) + '@' + $dcs);
     Write-Host('UserPricipalName updated to ' + $userinfo.SamAccountName + '@' + $dcs)
 } else {
     Write-Host('UserPricipalName is correct!')
