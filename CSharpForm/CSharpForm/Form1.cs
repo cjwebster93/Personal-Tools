a0f8d824558d2637e1d2a2f3b4e01fbc0e6959f0
Craig Webster <craigwebster14@gmail.com>
2020-08-12 11:22:24 +0100

    C# Form Practice

---
+++ CSharpForm/CSharpForm/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpForm
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void helloButton_Click(object sender, EventArgs e)
+        {
+            welcomeText.Text = "Hello " + nameText.Text;
+        }
+    }
+}
