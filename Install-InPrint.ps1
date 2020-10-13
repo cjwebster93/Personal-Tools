c167fb5e8360b430df359ec24fc06cd2578b8bfe
Craig Webster <craigwebster14@gmail.com>
2020-10-13 22:22:00 +0100

    Added Transcript, and more logging info

--- Install-InPrint.ps1
+++ Install-InPrint.ps1
@@ -1,24 +1,30 @@
+#Start transcript with identifiable log name
+$Path = Get-Location
+$LogPath = ("$Path\" + 'InPrintInstall.log')
+Write-Host('Transcript log file will be: ' + $LogPath)
+Start-Transcript -Path $LogPath
+
 #Check if installed
 $Installed = Test-Path -Path "C:\Program Files (x86)\Communication InPrint"
 $Date = Get-Date -UFormat "%d/%m/%Y %R"
-$Components = {
-    core,
-    InPrint,
-    inprint_resources_uk,
-    wordlistmanager
-}
+$Components = @('core','InPrint','inprint_resources_uk','wordlistmanager')
+
 Write-Host("Inprint Installed: $Installed")
-If ($Installed -eq 'False') 
-{   Foreach ($part in $Components)
+If (!$Installed) 
+{
+    Write-Host -ForegroundColor Yellow ("Starting installation at $Date")
+    Foreach ($part in $Components)
     {
         try 
         {
             Write-Host("Installing $part")
-            "msiexec.exe /i $part.msi /qn"
+            msiexec.exe /i "$Path\$part.msi" /qn
         }
         catch
         {
             Write-Host ("Error installing $part")
         }   
     }
+    Write-Host -ForegroundColor Green ("All packages installed!")
 }
+Stop-Transcript
