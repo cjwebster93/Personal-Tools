6e24382dad8d2bd82affc77315301006b2eb1db9
Craig Webster <craigwebster14@gmail.com>
2020-10-13 19:09:29 +0100

    Working on checks

--- Install-InPrint.ps1
+++ Install-InPrint.ps1
@@ -1,9 +1,20 @@
-#Installing packages in order
-Write-Host("Installing Core package")
-msiexec /i .\core.msi /qn
-Write-Host("Installing InPrint package")
-msiexec /i .\inprint.msi /qn
-Write-Host("Installing Resources package")
-msiexec /i .\inprint_resources_uk.msi /qn
-Write-Host("Installing Wordlist package")
-msiexec /i .\wordlistmanager.msi /qn
+#Check if installed
+$Installed = Test-Path -Path "C:\Program Files (x86)\Communication InPrint"
+$Date = Get-Date -UFormat "%d/%m/%Y %R"
+Write-Host("Inprint Installed: $Installed")
+If ($Installed -eq 'False') 
+{
+    Write-Host("Installing Core package")
+    try {
+        msiexec.exe /i .\core.msi /qn
+    }
+    catch 
+    Write-Host("Installing InPrint package")
+    msiexec.exe /i .\inprint.msi /qn
+    Write-Host("Installing Resources package")
+    msiexec.exe /i .\inprint_resources_uk.msi /qn
+    Write-Host("Installing Wordlist package")
+    msiexec.exe /i .\wordlistmanager.msi /qn
+    
+    Write-Host("Install Complete at $Date")
+}
